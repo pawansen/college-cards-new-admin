6c9e3040c5d0f8db00ade5c5a2138dec10ff6fea
Pawan Sen <pawan@vebuin.com>
2025-08-15 10:20:32 +0100

    up

--- src/services/apiPath.js
+++ src/services/apiPath.js
@@ -13,5 +13,5 @@ export const apiPath = {
     promoCodes: "/get-promo-code"
 };
 
-// export const APP_URL = "http://13.235.91.193:3009/v1/admin";
-export const APP_URL = "http://localhost:3009/v1/admin";
+export const APP_URL = "http://13.235.91.193:3009/v1/admin";
+//export const APP_URL = "http://localhost:3009/v1/admin";
